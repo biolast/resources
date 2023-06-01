4be173b489742143785c4e190d47211b23328b22
blobfysh <47480729+blobfysh@users.noreply.github.com>
2023-06-01 07:09:37 -0400

    remove regions index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as debuffs from './debuffs.js'
-import * as regions from './regions/index.js'
+import { Farm } from './regions/farm.js'
+import { Forest } from './regions/forest.js'
+import { Mall } from './regions/mall.js'
+import { Prison } from './regions/prison.js'
+import { Suburbs } from './regions/suburbs.js'
 
+
+export const regions = { Suburbs, Farm, Forest, Mall, Prison }
 
 export type DebuffType = typeof debuffs[keyof typeof debuffs]['name']
 export type RegionName = typeof regions[keyof typeof regions]['name']
@@ -12,6 +18,5 @@ export const isValidRegion = (l: string): l is RegionName => l in allRegions.map
 
 export * as Constants from './constants.js'
 export {
-	debuffs,
-	regions
+	debuffs
 }
--- src/regions/index.ts
+++
@@ -1,8 +0,0 @@
-import { Farm } from './farm.js'
-import { Forest } from './forest.js'
-import { Mall } from './mall.js'
-import { Prison } from './prison.js'
-import { Suburbs } from './suburbs.js'
-
-
-export { Suburbs, Farm, Forest, Mall, Prison }
